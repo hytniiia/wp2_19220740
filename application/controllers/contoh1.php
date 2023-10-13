17634dfbe7d4427d7fd3403f03ab1e67f05bdd6b
hytniiia <hytannspam@gmail.com>
2023-10-13 15:34:29 +0700

    penambahan file baru

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,8 +4,8 @@ class contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Imam Nawawi
-        Saya tingga di daerah Ciputat
+        echo"Nama saya kim taehyung
+        Saya tinggal di daerah Seoul
         olah raga yang saya sukai adalah
         Bulutangkis";
     }
